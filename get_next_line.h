1eedbb0bd041c86be92284a27d19e89860cf1c58
Ugo <ugo@MacBook-Air-4.local>
2024-11-21 11:24:18 +0100

    Optimisation et revision du code

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ufalzone <ufalzone@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 16:01:24 by ufalzone          #+#    #+#             */
-/*   Updated: 2024/11/21 11:23:20 by ufalzone         ###   ########.fr       */
+/*   Updated: 2024/11/21 11:24:05 by ufalzone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ int		ft_lenbuffer(char *str);
 char	*extract_line(char *stock);
 char	*update_stock(char *stock);
 char	*get_next_line(int fd);
-int		init_stock(char **stock);
+char	*init_stock(char **stock);
 char	*read_and_join(int fd, char *stock, char *buffer, int *bytes_read);
 
 
